c075a36b1f66807fab1a51d92ab30ef8b14b5c07
amyjtechwriter <61687663+amyjtechwriter@users.noreply.github.com>
2023-01-13 21:21:30 +0000

    Fixed array returning nothing

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,41 +1,85 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
+// import "./FormattedDate";
 import "./Weather";
-import WeatherForecastDay from "./WeatherForecastDay";
+// import "./WeatherInfo";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
-
   function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
+    console.log(response.data);
   }
+  console.log(props);
+  const apiKey = "0493f9ao53439abfde184ef87beca0tb";
+  var lon = props.data.lon;
+  var lat = props.data.lat;
+  var unit = "metric";
+  var apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=${unit}`;
+  axios.get(apiURL).then(handleResponse);
 
-  if (loaded) {
-    return (
-      <div className="WeatherForecast">
-        <div className="container-md text-center">
-          <div className="row">
-            {forecast.map(function (dailyForecast, index) {
-              if (index < 5)
-                return (
-                  <div className="col" key={index}>
-                    <WeatherForecastDay data={dailyForecast} />
-                  </div>
-                );
-            })}
+  return (
+    <div className="WeatherForecast">
+      <div className="container-md text-center">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">Monday</div>
+            <WeatherIcon
+              className= "icon"
+              url={props.data.icon_url}
+              description={props.data.description}
+            />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperature-min">20</span>
+              <span className="WeatherForecast-temperature-max">25</span>
+            </div>
+          </div>
+          <div className="col">
+            Tuesday
+            <WeatherIcon
+              url={props.data.icon_url}
+              description={props.data.description}
+            />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperature-min">20</span>
+              <span className="WeatherForecast-temperature-max">25</span>
+            </div>
+          </div>
+          <div className="col">
+            Wednesday
+            <WeatherIcon
+              url={props.data.icon_url}
+              description={props.data.description}
+            />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperature-min">20</span>
+              <span className="WeatherForecast-temperature-max">25</span>
+            </div>
+          </div>
+          <div className="col">
+            Thursday
+            <WeatherIcon
+              url={props.data.icon_url}
+              description={props.data.description}
+            />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperature-min">20</span>
+              <span className="WeatherForecast-temperature-max">25</span>
+            </div>
+          </div>
+          <div className="col">
+            Friday
+            <WeatherIcon
+              url={props.data.icon_url}
+              description={props.data.description}
+            />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperature-min">20</span>
+              <span className="WeatherForecast-temperature-max">25</span>
+            </div>
           </div>
         </div>
       </div>
-    );
-  } else {
-    const apiKey = "0493f9ao53439abfde184ef87beca0tb";
-    var city = props.data.city;
-    const unit = "metric";
-    const apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${unit}`;
-    axios.get(apiURL).then(handleResponse);
-    return null;
-  }
+    </div>
+  );
 }
